6f6900611877f20c9c4972c539bc2d87df5f7997
khyojun <nandong5923@naver.com>
2022-12-22 16:12:43 +0900

    feat: 협력하는 객체의 형태로 구성

---
+++ src/main/java/org/example/Movie.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javax.management.Query;
+
+public class Movie {
+
+    public Money getFee() {
+
+
+    }
+
+    public Query calculateMovieFee(Screening screening) {
+
+
+    }
+}
